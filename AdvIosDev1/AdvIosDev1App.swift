74994f18e713991570ee08edb2fc522fb9bfba8b
Seong-Jin-Woo <82586218+Seong-Jin-Woo@users.noreply.github.com>
2024-09-12 01:31:09 +1000

    Initial Commit

---
+++ AdvIosDev1/AdvIosDev1App.swift
@@ -0,0 +1,17 @@
+//
+//  AdvIosDev1App.swift
+//  AdvIosDev1
+//
+//  Created by Matthew Cruz on 12/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct AdvIosDev1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
